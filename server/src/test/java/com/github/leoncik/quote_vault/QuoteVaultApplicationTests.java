117feef6146626e48ced9774d404b1b2b20f6e8c
Léonard Wojcik <leonardw@tutanota.com>
2025-09-19 10:37:09 +0200

    build(server): init server

---
+++ server/src/test/java/com/github/leoncik/quote_vault/QuoteVaultApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.leoncik.quote_vault;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuoteVaultApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
