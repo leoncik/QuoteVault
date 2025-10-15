e3890284f1e12cdc36bc534046e556a2f498ec3d
Léonard Wojcik <leonardw@tutanota.com>
2025-10-15 20:17:27 +0200

    feat(server): set up quote ressource and TypeOrm

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { QuoteModule } from './quote/quote.module';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'admin',
+      password: 'password123',
+      database: 'quote_vault_db',
+      entities: [],
+      synchronize: true,
+    }),
+    QuoteModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
