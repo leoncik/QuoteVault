17070365ae603dff4b06c1a25c6469aa4bdfb924
Léonard Wojcik <leonardw@tutanota.com>
2025-10-17 16:33:57 +0200

    refactor(server): delete unused files

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,6 +1,4 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { QuoteModule } from './quote/quote.module';
 import { Quote } from './quote/entities/quote.entity';
@@ -20,7 +18,7 @@ import { Quote } from './quote/entities/quote.entity';
     }),
     QuoteModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
